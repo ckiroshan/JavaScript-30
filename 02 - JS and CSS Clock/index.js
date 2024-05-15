24f03ca3468afd742127f6fc741af12675b24ec8
Your <ckiroshan@gmail.com>
2024-05-15 11:31:44 +0530

    Created setDate function to update clock hands.

---
+++ 02 - JS and CSS Clock/index.js
@@ -0,0 +1,24 @@
+// Selectors
+const secondHand = document.querySelector(".second-hand");
+const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+  // Seconds
+  const seconds = now.getSeconds();
+  const secondDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+  // Minutes
+  const minutes = now.getMinutes();
+  const minuteDegrees = ((minutes / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+  // Minutes
+  const hours = now.getHours();
+  const hourDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
