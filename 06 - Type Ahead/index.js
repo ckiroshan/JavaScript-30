f9a9e13f200b15088f60c6ef0aa67fc2eba9c2e1
Your <ckiroshan@gmail.com>
2024-05-19 23:06:50 +0530

    Implement city search with highlighting & formatted population.

--- 06 - Type Ahead/index.js
+++ 06 - Type Ahead/index.js
@@ -9,3 +9,33 @@ fetch(endpoint) // Returns a promise
   .then((blob) => blob.json())
   .then((data) => cities.push(...data));
 
+// filters array based on keyword searched
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+// Modifies population values with commas
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Displays this list on HTML
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+      const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+      return `
+    <li>
+      <span class="name">${cityName}, ${stateName}</span>
+      <span class="population">${numberWithCommas(place.population)}</span>
+    </li>`;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
