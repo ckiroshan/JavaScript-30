c0f975c885b7d268f8184d67fa6763ac9b24b412
Your <ckiroshan@gmail.com>
2024-05-20 23:23:50 +0530

    Used some & every methods on people Array.

---
+++ 07 - Array Cardio Day 2/index.js
@@ -0,0 +1,40 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const isAdult = people.some(function (person) {
+  const date = new Date().getFullYear();
+  if (date - person.year >= 19) {
+    return true;
+  }
+});
+
+const isAdult2 = people.some((person) => {
+  const date = new Date().getFullYear();
+  return date - person.year >= 19 ? true : false;
+});
+
+console.log(isAdult2);
+
+// Array.prototype.every() // is everyone 19 or older?
+const isAllAdults = people.every((person) => {
+  const date = new Date().getFullYear();
+  return date - person.year >= 19 ? true : false;
+});
+
+console.log(isAllAdults);
