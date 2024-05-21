be944eb8e5ec3c069a90d987720890002bd29634
Your <ckiroshan@gmail.com>
2024-05-21 18:51:29 +0530

    Added event listeners to capture mouse events.

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -42,3 +42,15 @@ function draw(e) {
   direction === true ? ctx.lineWidth++ : ctx.lineWidth--;
 }
 
+// Event listeners
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]; // Destructuring
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+canvas.addEventListener("mouseout", () => {
+  isDrawing = false;
+});
